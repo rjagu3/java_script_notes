d1779f92b694f0360611c0ab77e51ddb892985d7
rjagu3 <165783226+rjagu3@users.noreply.github.com>
2024-05-01 17:49:57 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,19 @@
+// let will not allow re-declaration of variables
+let a=10
+console.log(a)
+
+//to know data type use typeof()
+
+console.log(typeof(a))
+
+//var can allow to re-declare the variables
+var a=20
+console.log(a)
+
+var a=30
+console.log(a)
+
+//const used to define constant variable which values cannot be modified during the program
+
+const b=20
+console.log(b)
